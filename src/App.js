41af94c619e27257b85f5dba5bca09b175f23738
dsanderson90 <dsanderson3@cnm.edu>
2018-12-28 12:25:07 -0700

    added the todolist component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Menu from './Menu/Menu'
+import TodoList from "./TodoList/TodoList";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-             This is my <p className="react"> react </p> playground
-          </p>
-        </header>
-			<Menu />
+      <div>
+			<TodoList/>
       </div>
     );
   }
