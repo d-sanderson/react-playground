e9a7e210315433f7c88165ec7f917050602d6ca2
dsanderson90 <dsanderson3@cnm.edu>
2019-01-02 23:12:56 -0700

    minor refactoring

--- src/MadLibs/MadLibs.js
+++ src/MadLibs/MadLibs.js
@@ -4,6 +4,7 @@ class MadLibs extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			headline: 'How to date the Coolest Person in School.',
 			pluralNoun1: '',
 			adverb1: '',
 			verb1: '',
@@ -38,7 +39,7 @@ class MadLibs extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>How to date the Coolest Person in School. </h1>
+				<h1>{this.state.headline} </h1>
 				<p> It is simple turn the {this.state.pluralNoun1}. Make him/her want {this.state.adverb1} to date you. Make sure you are always dressed to {this.state.verb1}. Each and
 				everyday wear a an {this.state.articleOfClothing} that you know shows of your {this.state.bodyPart1} to {this.state.adjective1} advantage and make
 				your {this.state.noun1} look like a million {this.state.pluralNoun2} even if the two of you make
